cba96e3d9aa671b36d6b6202369e4f2e4a221910
Vitor Souza <vitorx1280@gmail.com>
2021-11-26 01:43:35 -0300

    fix: user validation

--- src/database/database.js
+++ src/database/database.js
@@ -2,5 +2,6 @@ const databaseInfo = require('./database.info')
 const mysql = require('mysql')
 
 const connection = mysql.createConnection( databaseInfo )
+connection.end()
 
 module.exports = connection
