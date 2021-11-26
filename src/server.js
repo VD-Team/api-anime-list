a8c8f3f7f382a4e37e803dc4edf4023235557149
Vitor Souza <vitorx1280@gmail.com>
2021-11-26 00:39:54 -0300

    feat: redirect OPTIONS requests

--- src/server.js
+++ src/server.js
@@ -2,5 +2,17 @@ const express = require('express')
 const app = express()
 
 app.use(express.json())
+app.use(function(req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    // handle OPTIONS method
+    if ('OPTIONS' == req.method) {
+        return res.sendStatus(200);
+    } else {
+        next();
+    }
+});
 
 module.exports = app
