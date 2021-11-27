2563a661c9ad48eb9829b0354420b08790f408f1
Vitor Souza <vitorx1280@gmail.com>
2021-11-27 09:28:24 -0300

    fix: server config

--- src/server.js
+++ src/server.js
@@ -3,11 +3,7 @@ const app = express()
 
 app.use(express.json())
 app.use(function(req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', '*');
-    res.setHeader('Access-Control-Allow-Credentials', true);
-    // handle OPTIONS method
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');    // handle OPTIONS method
     if ('OPTIONS' == req.method) {
         return res.sendStatus(200);
     } else {
